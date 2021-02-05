06c50f4c27cf8a1c3de3ec7ad47e7417eca4baa6
Trent Patterson <jtrent238@outlook.com>
2021-02-05 15:59:11 -0500

    Made changes requested in #7 (#8)
    * Added coin flip
    
    * Update README.md

--- rat_bot.py
+++ rat_bot.py
@@ -11,6 +11,7 @@ client = discord.Client()
 
 activity_type = discord.ActivityType.listening
 activity_name = "squeaks"
+prefix = "!"
 
 ball_choices = (
     "It is certain.",
@@ -35,11 +36,14 @@ ball_choices = (
     "Very doubtful."
 )
 
+coin_choices = ( "Heads", "Tails")
+
 # Define events
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     print(f'Setting presence to {activity_name}')
+    print('Prefix is ' + '"' + prefix + '"')
     
     activity = discord.Activity(type=activity_type, name=activity_name)
     await client.change_presence(activity=activity)
@@ -48,7 +52,7 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
+    
     # Rat emoji function
     lower_case_message = message.content.lower()
     lower_case_message = ''.join(lower_case_message.split())
@@ -58,14 +62,30 @@ async def on_message(message):
         rat_num = rat_message.count("rat")
         rat_num += rat_message.count("671793984435126277")      
         await message.channel.send(':rat: ' * rat_num)
-
+            
     # Magic 8 Ball function
-    if message.content.startswith("!oracle"):
+    if message.content.startswith(prefix + "oracle"):
         # Thanks @PlusReed this is much better
         try:
             ball_result = choice(ball_choices)
             await message.channel.send(
                 "Squeek squeek! ({0})".format(ball_result),
+                mention_author=True, # Mention the user
+                reference=message # Set message_reference to the message.
+            )
+        except:
+            await message.channel.send(
+                "Squeek squeek... (Something went wrong, make <@457637280539082763> fix it...",
+                mention_author=True,
+                reference=message
+            )
+            
+    # Coin function
+    if message.content.startswith(prefix + "coin"):
+        try:
+            coin_result = choice(coin_choices)
+            await message.channel.send(
+                "Squeek squeek! :coin: ({0})".format(coin_result),
                 mention_author=True, # Mention the user
                 reference=message # Set message_reference to the message.
             )
